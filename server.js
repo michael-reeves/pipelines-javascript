99bf2625b0d14695fa7e8cd88153f230252d0f11
Michael Reeves <md.reeves@icloud.com>
2019-02-07 09:31:34 -0700

    Fix typo in response

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send( hello: process.env.HELLO );
+  res.send( { hello: process.env.HELLO } );
 });
 
 var port = process.env.PORT||PORT;
