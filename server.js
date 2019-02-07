e7cc5ba7175b39eeb7c61932615d0f9b70081cff
Michael Reeves <md.reeves@icloud.com>
2019-02-07 09:23:04 -0700

    Update / response to see if ENV variables are exposed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Michael, my name is Hal!\n');
+  res.send( hello: process.env.HELLO );
 });
 
 var port = process.env.PORT||PORT;
